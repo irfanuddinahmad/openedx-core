e19124db8aedabe9b47b6fa45769ef271964b806
irfanuddinahmad <34648393+irfanuddinahmad@users.noreply.github.com>
2026-09-09 14:07:24 +0500

    fix: remove unnecessary try/except around __version__ (not present pre-migration)

--- src/openedx_core/__init__.py
+++ src/openedx_core/__init__.py
@@ -4,11 +4,7 @@ Metadata for the openedx-core repository and its PyPI package.
 There is currently no public API for openedx_core--that's intentional!
 The public APIs belong to the specific apps (openedx_content, openedx_tagging, etc.).
 """
-from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as get_version
 
 # The version for the entire repository
-try:
-    __version__ = get_version("openedx-core")
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
+__version__ = get_version("openedx-core")
